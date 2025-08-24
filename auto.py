7a6215a67313590062023e6e79f4363b6e074615
zdenoiva <zdenoiva@gmail.com>
2025-08-24 19:12:35 +0200

    Update auto.py

--- auto.py
+++ auto.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(options=options)
 import time
 import os
 username_value = os.environ.get("USERNAME")
