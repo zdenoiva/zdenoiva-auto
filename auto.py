e6b9f51b4b7b5fdcafe819e502aba38baa9d3e17
zdenoiva <zdenoiva@gmail.com>
2025-08-24 19:57:58 +0200

    Update auto.py

--- auto.py
+++ auto.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 
@@ -28,7 +29,8 @@ def automatizacia():
     )
     
         # 2. Spustenie prehliadača (systémový chromedriver)
-    driver = webdriver.Chrome(options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     
     try:
         # ===========================================
