9e2cf432801709a8b25ccd784c411ee4ec5d5271
zdenoiva <zdenoiva@gmail.com>
2025-08-24 20:00:11 +0200

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import os
 
